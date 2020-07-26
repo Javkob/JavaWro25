1973e18b068a62925a029565345ad7b45463975a
W1tness <javkob.witness@gmail.com>
2020-07-26 14:48:59 +0200

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule} from "@angular/forms";
+import {environment} from "../environments/environment";
+import { LoginComponent } from './login/login.component';
+import {AngularFireModule} from "@angular/fire";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AngularFireModule.initializeApp(environment.firebase),
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
