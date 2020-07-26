7578cdbcd972aaa4f6ad65506e9ffc57b0900da0
W1tness <javkob.witness@gmail.com>
2020-07-26 12:30:51 +0200

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:  {
+    apiKey: "AIzaSyBzjGtPBqYtfuo7Jpca2Sw-ViSEyWaDrks",
+    authDomain: "javawro25-7f9ed.firebaseapp.com",
+    databaseURL: "https://javawro25-7f9ed.firebaseio.com",
+    projectId: "javawro25-7f9ed",
+    storageBucket: "javawro25-7f9ed.appspot.com",
+    messagingSenderId: "333833059350",
+    appId: "1:333833059350:web:8268bf7b0c8d2293c40937",
+    measurementId: "G-S0SQ3GN058"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:  {
+    apiKey: "AIzaSyBzjGtPBqYtfuo7Jpca2Sw-ViSEyWaDrks",
+    authDomain: "javawro25-7f9ed.firebaseapp.com",
+    databaseURL: "https://javawro25-7f9ed.firebaseio.com",
+    projectId: "javawro25-7f9ed",
+    storageBucket: "javawro25-7f9ed.appspot.com",
+    messagingSenderId: "333833059350",
+    appId: "1:333833059350:web:8268bf7b0c8d2293c40937",
+    measurementId: "G-S0SQ3GN058"
+  }
 };
 
 /*
