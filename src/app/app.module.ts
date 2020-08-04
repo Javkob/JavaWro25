9b846bd7326414ee7dfb2a153612b70205e22cc4
Bartek Pawzun <bartosz.pawzun@gmail.com>
2020-08-04 17:56:44 +0200

    added features and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { RestaurantListComponent } from './restaurant-list/restaurant-list.compo
 import {HttpClientModule} from '@angular/common/http';
 import {RestaurantDetailsComponent} from "./restaurant-details/restaurant-details.component";
 import {RegisterComponent} from './register/register.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
@@ -25,7 +26,8 @@ import {RegisterComponent} from './register/register.component';
     RestaurantListComponent,
     RestaurantDetailsComponent,
     RegisterComponent,
-    RestaurantListComponent
+    RestaurantListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
--- src/app/restaurant-details/restaurant-details.component.ts
+++ src/app/restaurant-details/restaurant-details.component.ts
@@ -9,17 +9,31 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class RestaurantDetailsComponent implements OnInit {
 
-  _meals:any=[]
+  _meals:any=[];
   restaurantName: any;
+  restaurantAddress: any;
+  _restaurants: any= [];
 
   constructor(private http:HttpClient, private route: ActivatedRoute) {
     this.restaurantName = route.snapshot.paramMap.get('name')
+    this.restaurantAddress = route.snapshot.paramMap.get('address')
+  // }
+  //
+  // ngOnInit(): void {
+  //   this.http.get(`https://jedzonkopl.herokuapp.com/restaurant/${(this.restaurantName)}`)
+  //     .toPromise()
+  //     .then((m) => {
+  //       this._meals = m;
+  //     })
   }
-
   ngOnInit(): void {
-    this.http.get(`https://jedzonkopl.herokuapp.com/restaurant/${(this.restaurantName)}`)
+    this.http.get("https://jedzonkopl.herokuapp.com/restaurants")
       .toPromise()
-      .then((m)=>{this._meals=m;})
+      .then((res)=>{this._restaurants=res;});
+
+    // this.http.get(`https://jedzonkopl.herokuapp.com/restaurant/${(this.restaurantAddress)}`)
+    //   .toPromise()
+    //   .then(()=>{this.restaurantAddress})
 
   }
 
